682f80463c04620d68f1b1bf8725cf272735b0b9
Velizar Nenov <vndepot-wdg@yahoo.com>
2024-04-17 17:51:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,31 @@ const
   numberOfRecords = process.argv[2] || 100,
   carBrands = require('./testdata.js');
 
+/**
+ * Waits for the specified number of milliseconds.
+ *
+ * @param {number} milliseconds - The number of milliseconds to wait.
+ * @returns {Promise<void>} - A promise that resolves after the specified number of milliseconds.
+ */
 function wait(milleseconds) {
   return new Promise(resolve => setTimeout(resolve, milleseconds))
 }
 
+/**
+ * Generates records with Redis commands asynchronously.
+ *
+ * @returns {Promise<void>} A promise that resolves when all records are generated.
+ */
 async function generateRecords() {
-  for (let i = 0; i < numberOfRecords; i++) {
+  const promises = Array.from({ length: numberOfRecords }, async (_, i) => {
     await wait(10);
     const randomBrandIndex = Math.floor(Math.random() * carBrands.length);
     const ts = (new Date()).getTime() + i + randomBrandIndex;
     const _brand = carBrands[randomBrandIndex];
     console.log(`HSET brandRankMonth:${ts} timestamp ${ts} brand "${_brand}"`);
     console.log(`EXPIRE brandRankMonth:${ts} ${86400 - randomBrandIndex} NX\n`);
-  }
+  });
+  await Promise.all(promises);
 }
 
 (async () => {
